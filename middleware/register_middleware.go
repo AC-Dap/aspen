b3d670a73f1613eb2a926f73d9e993a0523d744e
Anthony Cui <ninjaminer18@gmail.com>
2025-12-24 15:39:34 -0500

    Remove implicit typing

--- middleware/register_middleware.go
+++ middleware/register_middleware.go
@@ -3,6 +3,6 @@ package middleware
 import "aspen/config"
 
 func RegisterMiddleware() {
-	config.RegisterMiddlewareConstructor[LoggerParams]("logger", NewLogger)
-	config.RegisterMiddlewareConstructor[AuthParams]("auth", NewAuth)
+	config.RegisterMiddlewareConstructor("logger", NewLogger)
+	config.RegisterMiddlewareConstructor("auth", NewAuth)
 }
